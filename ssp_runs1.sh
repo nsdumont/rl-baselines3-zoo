e300cb50736b898b9f921bafe807198ec495e1c0
Nicole Dumont <nicole.s.dumont@gmail.com>
2024-02-15 15:30:38 -0500

    Correction

--- ssp_runs1.sh
+++ ssp_runs1.sh
@@ -1,8 +1,8 @@
-for env in "LunarLander-v2"  "Acrobot-v1"
+for env in "Acrobot-v1"
 do
     for (( seed = 0 ; seed < $1 ; ++seed ))
     do
         echo "Seed: $seed"
-        python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages hrr_gym_wrappers --conf-file hyperparams/ssp/ppo_ssp.yml --track --wandb-project-name ssp-rl --wandb-entity nicole-s-dumont --wandb-tags $env ssp-obs
+        python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages hrr_gym_wrappers --conf-file hyperparams/ssp/ppo_ssp.yml --track --wandb-project-name ssp-rl --wandb-entity nicole-s-dumont --wandb-tags $env new-ssp-obs
     done
 done
