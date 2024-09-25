d3befbdc9a657a26f8e0a4e709d84b475498b899
Nicole Dumont <nicole.s.dumont@gmail.com>
2024-09-25 15:09:10 -0400

    Update ssp_runs_conmaze.sh

--- ssp_runs_conmaze.sh
+++ ssp_runs_conmaze.sh
@@ -15,7 +15,7 @@ do
     for env in "ContinuousMaze-5x5-v0"  "ContinuousMazeBlocks-5x5-v0"  "ContinuousMaze-9x9-v0" "ContinuousMazeBlocks-9x9-v0"
     do
         echo $env
-        python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages gym_continuous_maze hrr_gym_wrappers --conf-file hyperparams/ssp/ppo_ssp.yml  --track --wandb-project-name sb3 --wandb-entity nicole-s-dumont --wandb-tags ssp-obs new
-        python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages gym_continuous_maze --conf-file hyperparams/ppo.yml  --track --wandb-project-name sb3 --wandb-entity nicole-s-dumont --wandb-tags default-obs new
+        python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages gym_continuous_maze hrr_gym_wrappers --conf-file hyperparams/learnssp/ppo_ssp.yml  --track --wandb-project-name ssp-continuous-maze --wandb-entity nicole-s-dumont --wandb-tags learnssp-obs
+        python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages gym_continuous_maze --conf-file hyperparams/ppo.yml  --track --wandb-project-name ssp-continuous-maze --wandb-entity nicole-s-dumont --wandb-tags default-obs
     done
 done
