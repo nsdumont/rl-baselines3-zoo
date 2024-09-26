f35a394f1f589abdc46d6f90a85603890d5a920e
Nicole Dumont <nicole.s.dumont@gmail.com>
2024-09-26 17:39:07 -0400

    Different scale range for continuous maze

--- ssp_runs_maze.sh
+++ ssp_runs_maze.sh
@@ -1,10 +1,10 @@
-for env in "maze-sample-5x5-v0" #"maze-sample-6x6-v0" "maze-sample-7x7-v0" "maze-sample-8x8-v0" "maze-sample-9x9-v0" "maze-sample-10x10-v0"
+for env in "ContinuousMaze-5x5-v0" "ContinuousMaze-7x7-v0"  "ContinuousMaze-10x10-v0"  "ContinuousMaze-12x12-v0"  "ContinuousMaze-15x15-v0" 
 do
     echo $env
     for (( seed = 0 ; seed < $1 ; ++seed ))
     do
         echo "Seed: $seed"
-        python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages gym_maze --conf-file hyperparams/ssp/ppo_ssp.yml --track --wandb-project-name ssp-rl --wandb-entity nicole-s-dumont --wandb-tags $env ssp-obs
-    	python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages gym_maze --conf-file hyperparams/ppo.yml  --track --wandb-project-name ssp-rl --wandb-entity nicole-s-dumont --wandb-tags $env default-obs 
+        python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages gym_continuous_maze hrr_gym_wrappers --conf-file hyperparams/learnssp/ppo_ssp.yml --hyperparams learning_rate:0.001 --track --wandb-project-name ssp-continuous-maze --wandb-entity nicole-s-dumont --wandb-tags ssp-obs
+    	python train.py --env $env --algo ppo --seed $seed --verbose 0 --gym-packages gym_continuous_maze --conf-file hyperparams/ppo.yml --track --wandb-project-name ssp-continuous-maze --wandb-entity nicole-s-dumont --wandb-tags default-obs 
     done
 done
